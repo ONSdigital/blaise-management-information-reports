41988e8c38db942a9c9ee403a78c1ad14ccef3cd
James Anthony Williams <81305008+JAWilliamsONS@users.noreply.github.com>
2022-05-05 09:37:02 +0100

    tests: write render tests for instrument filter
    reviewed-by: James & Jamie
    refs: BLAIS5-3036

--- src/reports/InterviewerCallHistory/InterviewerCallHistory.test.tsx
+++ src/reports/InterviewerCallHistory/InterviewerCallHistory.test.tsx
@@ -1,12 +1,12 @@
-//TODO:
-// Write unit/Gherkin test for rendering screen when no instrument found - get user requirements from Beth
-
 /**
  * @jest-environment jsdom
  */
 
 import "@testing-library/jest-dom";
 import React from "react";
+
+//TODO:
+// Write unit/Gherkin test for rendering screen when no instrument found - get user requirements from Beth
 
 describe("placeholder test - to be replaced by a ", () => {
     it("render a warning when no instruments are found test", async () => {
