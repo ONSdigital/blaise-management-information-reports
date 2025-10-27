dc85b097149aa1abe12dbe722b5f6d3b3e8a24e6
lambeb <141648830+lambeb@users.noreply.github.com>
2025-10-27 10:14:32 +0000

    updating new restapi client id for testing

--- tests/global-setup.ts
+++ tests/global-setup.ts
@@ -16,7 +16,7 @@ async function getIapToken(audience: string): Promise<string> {
 async function globalSetup(config: FullConfig) {
     console.log('Running global setup to fetch IAP token...');
     try {
-        const iapToken = await getIapToken("1034983553529-gapl7ndqce23gdtra82lc8di67eql2vl.apps.googleusercontent.com");
+        const iapToken = await getIapToken("1034983553529-kneoh4aras9ilr0h4ohplkg629ojggr5.apps.googleusercontent.com");
         
         // Save the token to a file. This is the standard way to pass data
         // from globalSetup to your tests.
