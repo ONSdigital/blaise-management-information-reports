87685222dc0904bb31d66b4dd843525a70b65a38
SidraJaved <16195224+SidraJaved@users.noreply.github.com>
2026-07-23 12:21:05 +0100

    github advanced security fix,

--- src/client/reports/filters/AppointmentQuestionnaireFilter.test.tsx
+++ src/client/reports/filters/AppointmentQuestionnaireFilter.test.tsx
@@ -27,6 +27,7 @@ function DateHelper() {
   const lastYear = new Date(today);
 
   lastYear.setFullYear(today.getFullYear() - 1);
+  return lastYear.toUTCString();
 }
 
 describe("the interviewer details page renders correctly", () => {
@@ -52,7 +53,7 @@ describe("the interviewer details page renders correctly", () => {
           questionnaires={["LMS2101_AA1"]}
           setQuestionnaires={setQuestionnaires}
           submitFunction={submit}
-          navigateBack={() => {}}
+          navigateBack={() => { }}
         />
       </MemoryRouter>,
     );
